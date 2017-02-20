591fe1001be0fd7fc79c1c218ddf14b8d14f3a06
cherr000 <chudue@hotmail.com>
2017-02-20 13:03:36 -0600

    First Upload

---
+++ LemonadeStand/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+        private void GetRule()
+        {
+            Console.WriteLine("Lemonade Stand");
+            Console.WriteLine("Rules You Will Start $10 Your Goal Is To Profit");
+        }
+    }
+}
